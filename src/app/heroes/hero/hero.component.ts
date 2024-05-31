87ff7c47332a51e59065aaaacb309ba01559fb38
krishee107 <krishee107@gmail.com>
2024-05-31 21:08:53 +0200

    Funciones y variables en los componentes

--- src/app/heroes/hero/hero.component.ts
+++ src/app/heroes/hero/hero.component.ts
@@ -8,4 +8,21 @@ import { Component } from '@angular/core';
 export class HeroComponent {
   public name: string = 'ironman';
   public age: number = 45;
+
+  get capitalizedName(): string {
+    return this.name.toUpperCase();
+  }
+
+  getHeroDescription(): string {
+    return `${this.name} is ${this.age} years old`;
+  }
+
+
+  changeHero(): void {
+    this.name = 'Spiderman';
+  }
+
+  changeAge(): void {
+    this.age = 20;
+  }
 }
