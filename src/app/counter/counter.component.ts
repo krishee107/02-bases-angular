bcb6d93e4c2708d7e1bea1cce6a7532b0ad22241
krishee107 <krishee107@gmail.com>
2024-05-31 20:33:42 +0200

    Crear componente nuevo + importarlo en module

---
+++ src/app/counter/counter.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  template: `Hola`, /* Se crea el template directamente en linea */
+  // templateUrl: './counter.component.html', // Se enlaza el template desde un archivo externo
+})
+export class CounterComponent {
+
+}
