71a4ae47e42bc0243fab075f178d8033c6ab8baf
krishee107 <krishee107@gmail.com>
2024-06-02 22:29:54 +0200

    Recibir informacion al componente desde el padre

--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -1,12 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Character } from '../interfaces/character.interface';
 
 @Component({
   selector: 'app-dbz-main-page',
   templateUrl: './main-page.component.html',
 })
 
-export class MainPageComponent implements OnInit {
-  constructor() { }
+export class MainPageComponent {
+  public characters: Character[] = [
+    { name: 'Goku', power: 15000 },
+    { name: 'Vegeta', power: 7500 },
+    { name: 'Trunks', power: 6000 },
 
-  ngOnInit() { }
+  ];
 }
