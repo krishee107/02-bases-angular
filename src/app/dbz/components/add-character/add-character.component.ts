5ec8a73b6fa4d3df464d1aad1a3035d949baa077
krishee107 <krishee107@gmail.com>
2024-06-02 22:19:31 +0200

    Separar page por componentes

---
+++ src/app/dbz/components/add-character/add-character.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'dbz-add-character',
+  templateUrl: './add-character.component.html',
+  styleUrls: ['./add-character.component.css']
+})
+export class AddCharacterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
